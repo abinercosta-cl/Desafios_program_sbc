e0438e828cf0bcedb8890ad9b2dbfce1b45e1d0a
abinercosta-cl <abinerclcosta@gmail.com>
2025-06-09 20:17:01 -0300

    adcionado arquivos iniciais de testes praticos em python

---
+++ Aula_06_jun/python/condicionais.py
@@ -0,0 +1,14 @@
+numero = int(input("Digite um número: "))
+
+if numero <= 100:
+    print("O número esta proximo a 100!.")
+else:
+    print("O numero não esta proximo a 100!!")
+
+# Verificar se o numero é positivo ou negativo
+if numero > 0:
+    print("o numero é positivo")
+else:
+    print(" o numero {numero} é negativo")
+
+print(numero, "é um número inteiro")
---
+++ Aula_06_jun/python/entrada_e_saida.py
@@ -0,0 +1,8 @@
+#entrada e saida em python Receba um valor de N e retorne a 
+n = int(input())#entrada
+
+#saida
+print("o numero que voce digitou foi", n)
+
+
+
