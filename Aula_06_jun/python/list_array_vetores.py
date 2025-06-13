5a654912a34c329aa924130593d2e6d39421bc22
abinercosta-cl <abinerclcosta@gmail.com>
2025-06-13 18:30:45 -0300

    Tratando questoes do readme com python e intalado ambiente virtual .venv

---
+++ Aula_06_jun/python/list_array_vetores.py
@@ -0,0 +1,13 @@
+import array
+import numpy as np
+# Exemplo de Lista em Python
+lista = [1, 2, 3, 4, 5]
+print("Lista:", lista)
+
+# Exemplo de Array usando o módulo array
+arr = array.array('i', [1, 2, 3, 4, 5])  # 'i' indica inteiros
+print("Array:", arr)
+
+# Exemplo de Vetor usando o módulo numpy
+vetor = np.array([1, 2, 3, 4, 5])
+print("Vetor (NumPy):", vetor)
